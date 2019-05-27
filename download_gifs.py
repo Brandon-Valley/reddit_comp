6c097f2c36790b027181ab73b2ca58184516acd9
Brandon Valley <bavvh8@mst.edu>
2019-05-27 12:50:26 -0500

    downloads top pic of r/pics

--- download_gifs.py
+++ download_gifs.py
@@ -1,4 +1,4 @@
-import praw
+import praw,requests,re
 
 reddit = praw.Reddit(client_id='0MND-O3qUZg0gw',
                      client_secret='XuwpmqtersoJVnbukddVFOgKXl4', 
@@ -6,12 +6,31 @@ reddit = praw.Reddit(client_id='0MND-O3qUZg0gw',
                      user_agent='PrawTut', 
                      username='goddard0001')
 
-subreddit = reddit.subreddit('python')    
+subreddit = reddit.subreddit('pics')    
 
-hot_python = subreddit.hot(limit=1)
+# hot_python = subreddit.hot(limit=1)
 
 # for submission in hot_python:
 #     print(submission)
+#     
+# for submission in hot_python:
+#     print(submission.title)
+#     
+
+posts = subreddit.hot(limit=2)
+
+
+for post in posts:
+    url = (post.url)
+    file_name = url.split("/")
+    if len(file_name) == 0:
+        file_name = re.findall("/(.*?)", url)
+    file_name = file_name[-1]
+    if "." not in file_name:
+        file_name += ".jpg"
+    print(file_name)
+r = requests.get(url)
+with open(file_name,"wb") as f:
+    f.write(r.content)
     
-for submission in hot_python:
-    print(submission.title)
+    
