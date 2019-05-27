8aff9e74519989c2e414c3d28e75aba3859276f2
Brandon Valley <bavvh8@mst.edu>
2019-05-27 12:44:26 -0500

    prints title of top post of subreddit

--- download_gifs.py
+++ download_gifs.py
@@ -1,1 +1,17 @@
-print('hi')
+import praw
+
+reddit = praw.Reddit(client_id='0MND-O3qUZg0gw',
+                     client_secret='XuwpmqtersoJVnbukddVFOgKXl4', 
+                     password='Barkbark1',
+                     user_agent='PrawTut', 
+                     username='goddard0001')
+
+subreddit = reddit.subreddit('python')    
+
+hot_python = subreddit.hot(limit=1)
+
+# for submission in hot_python:
+#     print(submission)
+    
+for submission in hot_python:
+    print(submission.title)
